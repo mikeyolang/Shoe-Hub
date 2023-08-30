f62ffee6d42f057c21defbf46ac1a92350e4aaf2
mikeyolang <brunomike965@gmail.com>
2023-08-30 23:43:49 +0300

    Creating the cart page

--- lib/UI/cartpage.dart
+++ lib/UI/cartpage.dart
@@ -1,17 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive/hive.dart';
+import 'package:shoehub/shared/appstyle.dart';
 
-class CartPage extends StatefulWidget {
-  const CartPage({super.key});
-
-  @override
-  State<CartPage> createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
+class CartPage extends StatelessWidget {
+  CartPage({super.key});
+  final _cartBox = Hive.box("cart_box");
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      
-    );
+    List<dynamic> cart = [];
+    final cartData = _cartBox.keys.map((key) {
+      final item = _cartBox.get(key);
+      return {
+        "id": key,
+        "name": item["name"],
+        "price": item["price"],
+        "imageUrl": item["imageUrl"],
+        "size": item["size"],
+        "quantity": item["quantity"],
+      };
+    }).toList();
+    cart = cartData.reversed.toList();
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(12),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                "My Cart",
+                style: appstyle(36, Colors.black, FontWeight.w500),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.65,
+                child: ListView.builder(
+                  itemCount: cart.length,
+                  padding: EdgeInsets.zero,
+                  itemBuilder: (context, index) {
+                    final data = cart[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                        child: Slidable(
+                          key: const ValueKey(0),
+                          endActionPane: ActionPane(
+                              motion: const ScrollMotion(),
+                              children: [
+                                SlidableAction(
+                                  onPressed: doNothing,
+                                  foregroundColor: Colors.white,
+                                  label: "Delete",
+                                  backgroundColor: Colors.red,
+                                  icon: Icons.delete,
+                                )
+                              ]),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 0.11,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade500,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.shade500,
+                                  spreadRadius: 5,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 1),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(12),
+                                      child: Image.asset(
+                                        data["imageUrl"],
+                                        width: 70,
+                                        height: 70,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 12, bottom: 20),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            data["name"],
+                                            style: appstyle(
+                                              18,
+                                              Colors.black,
+                                              FontWeight.w500,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
